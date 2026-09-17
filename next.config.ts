479ab2fa47fbced3a4ec9f71019a62d8f122b1d7
Hassaan-Anwar <hassaan.anwar20052@gmail.com>
2026-09-18 02:18:49 +0500

    added ngrok tunnel to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["amuser-rebate-viable.ngrok-free.dev"],
   /* config options here */
-};
+} as any;
 
 export default nextConfig;
